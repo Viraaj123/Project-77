6453f805a2f3451ed1b0063ab6f396a935057cef
Viraaj123 <91537902+Viraaj123@users.noreply.github.com>
2022-05-14 12:37:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+console.log("My console project");
+
+var first_name = "viraaj";
+var last_name = "bhirud";
+
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+
+var a = 12;
+var b = 9;
+
+var add = a + b ;
+console.log(add);
+
+var sub = a - b ;
+console.log(sub);
+
+var multiple = a * b ;
+console.log(multiple);
+
+var divide = a / b ;
+console.log(divide);
